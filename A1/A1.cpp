c9828996461b0159684afa4cfe63aba9d380abc7
Sanjay Gupta <sanjayguppy@icloud.com>
2024-01-18 22:16:44 -0500

    perfect sphere

--- A1/A1.cpp
+++ A1/A1.cpp
@@ -12,16 +12,18 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "sphere.hpp"
 
 using namespace glm;
 using namespace std;
 
 static const size_t DIM = 16;
+vector<glm::vec3> avatar_vertices;
 
 //----------------------------------------------------------------------------------------
 // Constructor
 A1::A1()
-	: current_col( 0 )
+	: current_col(0)
 {
 	colour[0] = 0.0f;
 	colour[1] = 0.0f;
@@ -31,7 +33,8 @@ A1::A1()
 //----------------------------------------------------------------------------------------
 // Destructor
 A1::~A1()
-{}
+{
+}
 
 //----------------------------------------------------------------------------------------
 /*
@@ -40,98 +43,85 @@ A1::~A1()
 void A1::init()
 {
 	// Initialize random number generator
-	int rseed=getpid();
+	int rseed = getpid();
 	srandom(rseed);
 	// Print random number seed in case we want to rerun with
 	// same random numbers
 	cout << "Random number seed = " << rseed << endl;
-	
 
 	// DELETE FROM HERE...
 	Maze m(DIM);
 	m.digMaze();
 	m.printMaze();
 	// ...TO HERE
-	
+
 	// Set the background colour.
-	glClearColor( 0.3, 0.5, 0.7, 1.0 );
+	glClearColor(0.3, 0.5, 0.7, 1.0);
 
 	// Build the shader
 	m_shader.generateProgramObject();
 	m_shader.attachVertexShader(
-		getAssetFilePath( "VertexShader.vs" ).c_str() );
+		getAssetFilePath("VertexShader.vs").c_str());
 	m_shader.attachFragmentShader(
-		getAssetFilePath( "FragmentShader.fs" ).c_str() );
+		getAssetFilePath("FragmentShader.fs").c_str());
 	m_shader.link();
 
 	// Set up the uniforms
-	P_uni = m_shader.getUniformLocation( "P" );
-	V_uni = m_shader.getUniformLocation( "V" );
-	M_uni = m_shader.getUniformLocation( "M" );
-	col_uni = m_shader.getUniformLocation( "colour" );
+	P_uni = m_shader.getUniformLocation("P");
+	V_uni = m_shader.getUniformLocation("V");
+	M_uni = m_shader.getUniformLocation("M");
+	col_uni = m_shader.getUniformLocation("colour");
 
 	initGrid();
 
 	// Set up initial view and projection matrices (need to do this here,
 	// since it depends on the GLFW window being set up correctly).
-	view = glm::lookAt( 
-		glm::vec3( 0.0f, 2.*float(DIM)*2.0*M_SQRT1_2, float(DIM)*2.0*M_SQRT1_2 ),
-		glm::vec3( 0.0f, 0.0f, 0.0f ),
-		glm::vec3( 0.0f, 1.0f, 0.0f ) );
+	view = glm::lookAt(
+		glm::vec3(0.0f, 2. * float(DIM) * 2.0 * M_SQRT1_2, float(DIM) * 2.0 * M_SQRT1_2),
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(0.0f, 1.0f, 0.0f));
 
-	proj = glm::perspective( 
-		glm::radians( 30.0f ),
-		float( m_framebufferWidth ) / float( m_framebufferHeight ),
-		1.0f, 1000.0f );
+	proj = glm::perspective(
+		glm::radians(30.0f),
+		float(m_framebufferWidth) / float(m_framebufferHeight),
+		1.0f, 1000.0f);
 }
 
 void A1::initGrid()
 {
-	size_t sz = 3 * 2 * 2 * (DIM+3);
-
-	float *verts = new float[ sz ];
-	size_t ct = 0;
-	for( int idx = 0; idx < DIM+3; ++idx ) {
-		verts[ ct ] = -1;
-		verts[ ct+1 ] = 0;
-		verts[ ct+2 ] = idx-1;
-		verts[ ct+3 ] = DIM+1;
-		verts[ ct+4 ] = 0;
-		verts[ ct+5 ] = idx-1;
-		ct += 6;
-
-		verts[ ct ] = idx-1;
-		verts[ ct+1 ] = 0;
-		verts[ ct+2 ] = -1;
-		verts[ ct+3 ] = idx-1;
-		verts[ ct+4 ] = 0;
-		verts[ ct+5 ] = DIM+1;
-		ct += 6;
+	Icosphere sphere;
+	sphere.generateIcosphere(1);
+	avatar_vertices = sphere.faces_vertices;
+	cout << "SIZE: " << avatar_vertices.size() << endl;
+	vec3 vertices[avatar_vertices.size()];
+	for (int i = 0; i < avatar_vertices.size(); i++)
+	{
+		vertices[i] = avatar_vertices[i];
 	}
+	// sphere.printVertices();
 
-	// Create the vertex array to record buffer assignments.
-	glGenVertexArrays( 1, &m_grid_vao );
-	glBindVertexArray( m_grid_vao );
+	glGenVertexArrays(1, &m_grid_vao);
+	glBindVertexArray(m_grid_vao);
 
 	// Create the cube vertex buffer
-	glGenBuffers( 1, &m_grid_vbo );
-	glBindBuffer( GL_ARRAY_BUFFER, m_grid_vbo );
-	glBufferData( GL_ARRAY_BUFFER, sz*sizeof(float),
-		verts, GL_STATIC_DRAW );
+	glGenBuffers(1, &m_grid_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_grid_vbo);
+	glBufferData(GL_ARRAY_BUFFER, avatar_vertices.size() * sizeof(vec3),
+				 vertices, GL_STATIC_DRAW);
 
 	// Specify the means of extracting the position values properly.
-	GLint posAttrib = m_shader.getAttribLocation( "position" );
-	glEnableVertexAttribArray( posAttrib );
-	glVertexAttribPointer( posAttrib, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+	GLint posAttrib = m_shader.getAttribLocation("position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	// Reset state to prevent rogue code from messing with *my* 
+	// Reset state to prevent rogue code from messing with *my*
 	// stuff!
-	glBindVertexArray( 0 );
-	glBindBuffer( GL_ARRAY_BUFFER, 0 );
-	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// OpenGL has the buffer now, there's no need for us to keep a copy.
-	delete [] verts;
+	// delete[] verts;
 
 	CHECK_GL_ERRORS;
 }
@@ -151,7 +141,7 @@ void A1::appLogic()
  */
 void A1::guiLogic()
 {
-	// We already know there's only going to be one window, so for 
+	// We already know there's only going to be one window, so for
 	// simplicity we'll store button states in static local variables.
 	// If there was ever a possibility of having multiple instances of
 	// A1 running simultaneously, this would break; you'd want to make
@@ -162,44 +152,47 @@ void A1::guiLogic()
 	ImGuiWindowFlags windowFlags(ImGuiWindowFlags_AlwaysAutoResize);
 	float opacity(0.5f);
 
-	ImGui::Begin("Debug Window", &showDebugWindow, ImVec2(100,100), opacity, windowFlags);
-		if( ImGui::Button( "Quit Application" ) ) {
-			glfwSetWindowShouldClose(m_window, GL_TRUE);
-		}
+	ImGui::Begin("Debug Window", &showDebugWindow, ImVec2(100, 100), opacity, windowFlags);
+	if (ImGui::Button("Quit Application"))
+	{
+		glfwSetWindowShouldClose(m_window, GL_TRUE);
+	}
 
-		// Eventually you'll create multiple colour widgets with
-		// radio buttons.  If you use PushID/PopID to give them all
-		// unique IDs, then ImGui will be able to keep them separate.
-		// This is unnecessary with a single colour selector and
-		// radio button, but I'm leaving it in as an example.
+	// Eventually you'll create multiple colour widgets with
+	// radio buttons.  If you use PushID/PopID to give them all
+	// unique IDs, then ImGui will be able to keep them separate.
+	// This is unnecessary with a single colour selector and
+	// radio button, but I'm leaving it in as an example.
 
-		// Prefixing a widget name with "##" keeps it from being
-		// displayed.
+	// Prefixing a widget name with "##" keeps it from being
+	// displayed.
 
-		ImGui::PushID( 0 );
-		ImGui::ColorEdit3( "##Colour", colour );
-		ImGui::SameLine();
-		if( ImGui::RadioButton( "##Col", &current_col, 0 ) ) {
-			// Select this colour.
-		}
-		ImGui::PopID();
+	ImGui::PushID(0);
+	ImGui::ColorEdit3("##Colour", colour);
+	ImGui::SameLine();
+	if (ImGui::RadioButton("##Col", &current_col, 0))
+	{
+		// Select this colour.
+	}
+	ImGui::PopID();
 
-/*
-		// For convenience, you can uncomment this to show ImGui's massive
-		// demonstration window right in your application.  Very handy for
-		// browsing around to get the widget you want.  Then look in 
-		// shared/imgui/imgui_demo.cpp to see how it's done.
-		if( ImGui::Button( "Test Window" ) ) {
-			showTestWindow = !showTestWindow;
-		}
-*/
+	/*
+			// For convenience, you can uncomment this to show ImGui's massive
+			// demonstration window right in your application.  Very handy for
+			// browsing around to get the widget you want.  Then look in
+			// shared/imgui/imgui_demo.cpp to see how it's done.
+			if( ImGui::Button( "Test Window" ) ) {
+				showTestWindow = !showTestWindow;
+			}
+	*/
 
-		ImGui::Text( "Framerate: %.1f FPS", ImGui::GetIO().Framerate );
+	ImGui::Text("Framerate: %.1f FPS", ImGui::GetIO().Framerate);
 
 	ImGui::End();
 
-	if( showTestWindow ) {
-		ImGui::ShowTestWindow( &showTestWindow );
+	if (showTestWindow)
+	{
+		ImGui::ShowTestWindow(&showTestWindow);
 	}
 }
 
@@ -211,26 +204,28 @@ void A1::draw()
 {
 	// Create a global transformation for the model (centre it).
 	mat4 W;
-	W = glm::translate( W, vec3( -float(DIM)/2.0f, 0, -float(DIM)/2.0f ) );
+	W = glm::translate(W, vec3(-float(DIM) / 2.0f, 0, -float(DIM) / 2.0f));
 
 	m_shader.enable();
-		glEnable( GL_DEPTH_TEST );
+	glEnable(GL_DEPTH_TEST);
 
-		glUniformMatrix4fv( P_uni, 1, GL_FALSE, value_ptr( proj ) );
-		glUniformMatrix4fv( V_uni, 1, GL_FALSE, value_ptr( view ) );
-		glUniformMatrix4fv( M_uni, 1, GL_FALSE, value_ptr( W ) );
+	glUniformMatrix4fv(P_uni, 1, GL_FALSE, value_ptr(proj));
+	glUniformMatrix4fv(V_uni, 1, GL_FALSE, value_ptr(view));
+	glUniformMatrix4fv(M_uni, 1, GL_FALSE, value_ptr(W));
 
-		// Just draw the grid for now.
-		glBindVertexArray( m_grid_vao );
-		glUniform3f( col_uni, 1, 1, 1 );
-		glDrawArrays( GL_LINES, 0, (3+DIM)*4 );
+	// Just draw the grid for now.
+	glBindVertexArray(m_grid_vao);
+	glUniform3f(col_uni, 1, 1, 1);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawArrays(GL_TRIANGLES, 0, avatar_vertices.size());
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-		// Draw the cubes
-		// Highlight the active square.
+	// Draw the cubes
+	// Highlight the active square.
 	m_shader.disable();
 
 	// Restore defaults
-	glBindVertexArray( 0 );
+	glBindVertexArray(0);
 
 	CHECK_GL_ERRORS;
 }
@@ -240,15 +235,16 @@ void A1::draw()
  * Called once, after program is signaled to terminate.
  */
 void A1::cleanup()
-{}
+{
+}
 
 //----------------------------------------------------------------------------------------
 /*
  * Event handler.  Handles cursor entering the window area events.
  */
-bool A1::cursorEnterWindowEvent (
-		int entered
-) {
+bool A1::cursorEnterWindowEvent(
+	int entered)
+{
 	bool eventHandled(false);
 
 	// Fill in with event handling code...
@@ -260,15 +256,16 @@ bool A1::cursorEnterWindowEvent (
 /*
  * Event handler.  Handles mouse cursor movement events.
  */
-bool A1::mouseMoveEvent(double xPos, double yPos) 
+bool A1::mouseMoveEvent(double xPos, double yPos)
 {
 	bool eventHandled(false);
 
-	if (!ImGui::IsMouseHoveringAnyWindow()) {
+	if (!ImGui::IsMouseHoveringAnyWindow())
+	{
 		// Put some code here to handle rotations.  Probably need to
 		// check whether we're *dragging*, not just moving the mouse.
 		// Probably need some instance variables to track the current
-		// rotation amount, and maybe the previous X position (so 
+		// rotation amount, and maybe the previous X position (so
 		// that you can rotate relative to the *change* in X.
 	}
 
@@ -279,10 +276,12 @@ bool A1::mouseMoveEvent(double xPos, double yPos)
 /*
  * Event handler.  Handles mouse button events.
  */
-bool A1::mouseButtonInputEvent(int button, int actions, int mods) {
+bool A1::mouseButtonInputEvent(int button, int actions, int mods)
+{
 	bool eventHandled(false);
 
-	if (!ImGui::IsMouseHoveringAnyWindow()) {
+	if (!ImGui::IsMouseHoveringAnyWindow())
+	{
 		// The user clicked in the window.  If it's the left
 		// mouse button, initiate a rotation.
 	}
@@ -294,7 +293,8 @@ bool A1::mouseButtonInputEvent(int button, int actions, int mods) {
 /*
  * Event handler.  Handles mouse scroll wheel events.
  */
-bool A1::mouseScrollEvent(double xOffSet, double yOffSet) {
+bool A1::mouseScrollEvent(double xOffSet, double yOffSet)
+{
 	bool eventHandled(false);
 
 	// Zoom in or out.
@@ -306,7 +306,8 @@ bool A1::mouseScrollEvent(double xOffSet, double yOffSet) {
 /*
  * Event handler.  Handles window resize events.
  */
-bool A1::windowResizeEvent(int width, int height) {
+bool A1::windowResizeEvent(int width, int height)
+{
 	bool eventHandled(false);
 
 	// Fill in with event handling code...
@@ -318,11 +319,13 @@ bool A1::windowResizeEvent(int width, int height) {
 /*
  * Event handler.  Handles key input events.
  */
-bool A1::keyInputEvent(int key, int action, int mods) {
+bool A1::keyInputEvent(int key, int action, int mods)
+{
 	bool eventHandled(false);
 
 	// Fill in with event handling code...
-	if( action == GLFW_PRESS ) {
+	if (action == GLFW_PRESS)
+	{
 		// Respond to some key events.
 	}
 
---
+++ A1/sphere.hpp
@@ -0,0 +1,141 @@
+#include <glm/glm.hpp>
+#include <vector>
+#include <map>
+#include <iostream>
+
+struct Triangle
+{
+  int v1, v2, v3;
+  Triangle(int v1, int v2, int v3) : v1(v1), v2(v2), v3(v3) {}
+};
+
+class Icosphere
+{
+public:
+  std::vector<glm::vec3> vertices;
+  std::vector<glm::vec3> faces_vertices;
+  int index = -1;
+  std::map<std::pair<int, int>, int> hash_table;
+
+  // fix position of vertex to be on unit sphere, return index
+  int addVertex(float x, float y, float z)
+  {
+    float length = glm::sqrt(x * x + y * y + z * z);
+    vertices.push_back(glm::vec3(x / length, y / length, z / length));
+    return ++index;
+  }
+
+  // return index of point in the middle of p1 and p2
+  int getMiddlePoint(int v1, int v2)
+  {
+    std::pair<int, int> key = (v1 <= v2) ? std::make_pair(v1, v2) : std::make_pair(v2, v1);
+
+    if (hash_table.find(key) != hash_table.end())
+      return hash_table[key];
+
+    glm::vec3 vert1 = vertices[v1];
+    glm::vec3 vert2 = vertices[v2];
+
+    float x = (vert1.x + vert2.x) / 2.0;
+    float y = (vert1.y + vert2.y) / 2.0;
+    float z = (vert1.z + vert2.z) / 2.0;
+
+    int idx = addVertex(x, y, z);
+
+    hash_table[key] = idx;
+    return idx;
+  }
+
+public:
+  void generateIcosphere(int recursionLevel)
+  {
+
+    // create 12 vertices of a icosahedron
+    const float phi = (1.0 + glm::sqrt(5.0)) / 2.0;
+    addVertex(-1, phi, 0);
+    addVertex(1, phi, 0);
+    addVertex(-1, -phi, 0);
+    addVertex(1, -phi, 0);
+
+    addVertex(0, -1, phi);
+    addVertex(0, 1, phi);
+    addVertex(0, -1, -phi);
+    addVertex(0, 1, -phi);
+
+    addVertex(phi, 0, -1);
+    addVertex(phi, 0, 1);
+    addVertex(-phi, 0, -1);
+    addVertex(-phi, 0, 1);
+
+    std::vector<Triangle> faces;
+
+    faces.emplace_back(0, 11, 5);
+    faces.emplace_back(0, 5, 1);
+    faces.emplace_back(0, 1, 7);
+    faces.emplace_back(0, 7, 10);
+    faces.emplace_back(0, 10, 11);
+    faces.emplace_back(1, 5, 9);
+    faces.emplace_back(5, 11, 4);
+    faces.emplace_back(11, 10, 2);
+    faces.emplace_back(10, 7, 6);
+    faces.emplace_back(7, 1, 8);
+    faces.emplace_back(3, 9, 4);
+    faces.emplace_back(3, 4, 2);
+    faces.emplace_back(3, 2, 6);
+    faces.emplace_back(3, 6, 8);
+    faces.emplace_back(3, 8, 9);
+    faces.emplace_back(4, 9, 5);
+    faces.emplace_back(2, 4, 11);
+    faces.emplace_back(6, 2, 10);
+    faces.emplace_back(8, 6, 7);
+    faces.emplace_back(9, 8, 1);
+    int k = 0;
+
+    for (int i = 0; i < recursionLevel; i++)
+    {
+      std::vector<Triangle> new_faces;
+      for (auto triangle : faces)
+      {
+        // recursive step, split triangle into 4 smaller triangles
+        // std::cout << "processing: " << triangle.v1 << " " << triangle.v2 << " " << triangle.v3 << std::endl;
+        int a = getMiddlePoint(triangle.v1, triangle.v2);
+        int b = getMiddlePoint(triangle.v2, triangle.v3);
+        int c = getMiddlePoint(triangle.v3, triangle.v1);
+
+        // std::cout << "new vertices: " << a << " " << b << " " << c << std::endl;
+        new_faces.emplace_back(triangle.v1, a, c);
+        new_faces.emplace_back(a, triangle.v2, b);
+        new_faces.emplace_back(c, b, triangle.v3);
+        new_faces.emplace_back(a, b, c);
+        ++k;
+        // if (k > 10)
+        //   break;
+      }
+      faces = new_faces;
+    }
+    // std::cout << "processed " << k << " faces." << std::endl;
+    for (auto triangle : faces)
+    {
+      faces_vertices.push_back(vertices[triangle.v1]);
+      faces_vertices.push_back(vertices[triangle.v2]);
+      faces_vertices.push_back(vertices[triangle.v3]);
+    }
+    int count = 0;
+    std::cout << "Total Vertices: " << vertices.size() << std::endl;
+    count = count / 2;
+    std::cout << "added " << count << " vertices; total: " << 12 + count << std::endl;
+  }
+  void printVertices()
+  {
+    std::cout << vertices.size() << std::endl;
+    for (auto vert : vertices)
+    {
+      std::cout << vert.x << " " << vert.y << " " << vert.z << std::endl;
+    }
+  }
+};
+
+// Recursion Depth 0 (Initial Icosahedron): 12 vertices.
+// Recursion Depth 1: 27.
+// Recursion Depth 2: 87.
+// Recursion Depth 3: 327.
